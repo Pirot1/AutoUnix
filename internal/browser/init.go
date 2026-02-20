7036dac2f48b3ed93d0039a997519573cd59071f
Timur <122035339+Pirot1@users.noreply.github.com>
2026-02-20 11:57:49 +0500

    Add: Initilisation browser

---
+++ internal/browser/init.go
@@ -0,0 +1,22 @@
+package browser
+
+import (
+	"fmt"
+
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/launcher"
+)
+
+func Init(login string, password string) (*rod.Browser, *rod.Page) {
+	l := launcher.New().Headless(false).Devtools(false).Leakless(false)
+	url, err := l.Launch()
+	if err != nil {
+		panic(fmt.Sprintf("Не удалось запустить браузер: %v", err))
+	}
+	browser := rod.New().ControlURL(url).MustConnect().NoDefaultDevice()
+	//defer browser.MustClose()
+
+	page := browser.MustPage("https://uni-x.almv.kz/platform/login")
+	fmt.Println("Начинаем работу...")
+	return browser, page
+}
