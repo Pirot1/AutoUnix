fc46040680e5da011897af6a9c4c76ff14c1dfca
Timur <122035339+Pirot1@users.noreply.github.com>
2026-02-26 13:50:13 +0500

    Add: add constant

--- internal/parser/sidebar.go
+++ internal/parser/sidebar.go
@@ -6,7 +6,11 @@ import (
 	"github.com/go-rod/rod"
 )
 
-func GetAvailableLessons(page *rod.Page, code string) []string {
+const (
+	code = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTYiIGhlaWdodD0iMTciIHZpZXdCb3g9IjAgMCAxNiAxNyIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTE2IDguNUMxNiAxMi45MTgzIDEyLjQxODMgMTYuNSA4IDE2LjVDMy41ODE3MiAxNi41IDAgMTIuOTE4MyAwIDguNUMwIDQuMDgxNzIgMy41ODE3MiAwLjUgOCAwLjVDMTIuNDE4MyAwLjUgMTYgNC4wODE3MiAxNiA4LjVaTTEuOTkyIDguNUMxLjk5MiAxMS44MTgxIDQuNjgxODcgMTQuNTA4IDggMTQuNTA4QzExLjMxODEgMTQuNTA4IDE0LjAwOCAxMS44MTgxIDE0LjAwOCA4LjVDMTQuMDA4IDUuMTgxODcgMTEuMzE4MSAyLjQ5MiA4IDIuNDkyQzQuNjgxODcgMi40OTIgMS45OTIgNS4xODE4NyAxLjk5MiA4LjVaIiBmaWxsPSIjQUZCN0NBIiBmaWxsLW9wYWNpdHk9IjAuNSIvPgo8ZWxsaXBzZSBjeD0iOCIgY3k9IjguNSIgcng9IjgiIHJ5PSI4IiB0cmFuc2Zvcm09InJvdGF0ZSgtOTAgOCA4LjUpIiBmaWxsPSIjM0JCQzMwIi8+CjxwYXRoIGQ9Ik00LjIwODUgOC40OTk4NEw2LjkxNjgzIDExLjIwODJMMTIuMzMzNSA1Ljc5MTUiIHN0cm9rZT0id2hpdGUiIHN0cm9rZS13aWR0aD0iMS41IiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4KPC9zdmc+Cg=="
+)
+
+func GetAvailableLessons(page *rod.Page) []string {
 	chapters := page.MustActivate().MustElementsX("//div[@class=\"mt-5 rounded-[28px] bg-white dark:bg-[#1a1a1a] p-6 flex-0 overflow-y-auto\"]//div[@class=\"flex flex-row items-start cursor-pointer\"]")
 	fmt.Println("Открываю главы...")
 	if len(chapters) > 1 {
