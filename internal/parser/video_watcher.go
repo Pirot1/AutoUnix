c45b1febce35f5a4890059e7ea6426d785dca661
Timur <122035339+Pirot1@users.noreply.github.com>
2026-02-27 00:18:40 +0500

    Add: Now can init quiz and answer questions

--- internal/parser/video_watcher.go
+++ internal/parser/video_watcher.go
@@ -4,7 +4,13 @@ import (
 	"fmt"
 	"time"
 
+	"AutoUnix/internal/ai"
+
 	"github.com/go-rod/rod"
+)
+
+const (
+	test string = "//div[@class=\"absolute video-overlay w-full h-full flex-col items-center text-center justify-center flex\"]//button[@class=\"bg-[#FFDD33] text-black font-bold rounded mt-3 px-2 py-1\"]"
 )
 
 func Proceed_lesson(page *rod.Page) {
@@ -37,12 +43,9 @@ func waitForVideoEnd(page *rod.Page) {
 	for {
 		result, err := page.Eval(`() => {
 			let v = document.getElementById('video');
-			
-			// Если видео нет или readyState === 0 (метаданные не загружены)
 			if (!v || v.readyState === 0) {
 				return { ready: false, ended: false, current: 0, total: 0 };
-			}
-			
+			}	
 			return {
 				ready: true,
 				ended: v.ended,
@@ -65,13 +68,33 @@ func waitForVideoEnd(page *rod.Page) {
 		current := result.Value.Get("current").Int()
 		total := result.Value.Get("total").Int()
 
-		fmt.Printf("\rВоспроизведение: %d сек. из %d сек.", current, total)
+		fmt.Printf("\rВоспроизведение: %d сек. из %d сек. 			%.0f%%", current, total, (float32(current)/float32(total))*100)
 
-		if ended || (total > 0 && current >= total-1) {
+		if ended || (total > 0 && current >= total) {
 			fmt.Println("\nВидео успешно досмотрено!")
+			HadlePostVideoActions(page)
 			break
 		}
 
-		time.Sleep(3 * time.Second)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func HadlePostVideoActions(page *rod.Page) {
+	fmt.Println("Анализирую страницу после видео...")
+	res, err := page.Eval(`() => {
+    const btn = Array.from(document.querySelectorAll('button')).find(b => b.innerText.includes('Go to test'));
+    if (btn) {
+        btn.click();
+        return true;
+    }
+    return false;
+	}`)
+
+	if err == nil && res.Value.Bool() {
+		fmt.Println("Перехожу на тест")
+		ai.SolvingQuiz(page)
+	} else {
+		fmt.Println("Теста нету")
 	}
 }
