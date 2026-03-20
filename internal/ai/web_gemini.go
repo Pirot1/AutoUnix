edb4b1716a9d9b1a8048ed89338ae43843c5352a
Timur <122035339+Pirot1@users.noreply.github.com>
2026-03-20 09:52:49 +0500

    Add: Add new function to make AI conspects

--- internal/ai/web_gemini.go
+++ internal/ai/web_gemini.go
@@ -35,3 +35,21 @@ func AskGemini_Web(question string, options []string) int {
 	res, _ := strconv.Atoi(result)
 	return res
 }
+
+func Make_AI_conspect(txt string) string {
+	b, page := browser.Init("https://gemini.google.com/app", true) // потом поставить false
+	defer b.MustClose()
+	fmt.Println("Успешно запустил Gemini")
+
+	prompt := fmt.Sprintf(
+		"Ты — помощник в обучении. Твоя задача: написать конспект на английском языке изходя из \"сырого\" файла конспекта:\n%s", txt,
+	)
+	page.MustElementX(`//div[@role="textbox"]`).MustInput(prompt)
+	page.KeyActions().Press(input.Enter).MustDo()
+	fmt.Println("Ввёл вопрос. Жду ответ...")
+	page.MustElementX(`//div[@class="container"]`).MustVisible()
+	time.Sleep(500 * time.Millisecond)
+	result := page.MustElementX(`//div[@class="markdown markdown-main-panel enable-updated-hr-color"]`).MustText()
+	time.Sleep(500 * time.Millisecond)
+	return result
+}
