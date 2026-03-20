b516d043883a95b9dc377140c5591e0092edbd2b
Timur <122035339+Pirot1@users.noreply.github.com>
2026-03-20 18:45:09 +0500

    Fix: fix issues with headless initialisation

--- internal/browser/init.go
+++ internal/browser/init.go
@@ -5,19 +5,29 @@ import (
 
 	"github.com/go-rod/rod"
 	"github.com/go-rod/rod/lib/launcher"
+	"github.com/go-rod/rod/lib/proto"
 	"github.com/go-rod/stealth"
 )
 
 func Init(site string, head bool) (*rod.Browser, *rod.Page) {
+	//Параметры инициализации
 	chromePath := `C:\Program Files\Google\Chrome\Application\chrome.exe`
-	l := launcher.New().Bin(chromePath).Headless(head).Devtools(false).Leakless(false).Set("autoplay-policy", "no-user-gesture-required")
+	l := launcher.New().Bin(chromePath).Headless(head).Devtools(false).Leakless(false)
+	l.Set("autoplay-policy", "no-user-gesture-required")
+	l.Set("disable-gpu")
+	l.Set("no-sandbox")
+	l.Set("disable-dev-shm-usage")
+	l.Set("disable-extensions")
 	url, err := l.Launch()
 	if err != nil {
 		panic(fmt.Sprintf("Не удалось запустить браузер: %v", err))
 	}
 	browser := rod.New().ControlURL(url).MustConnect().NoDefaultDevice()
-	//defer browser.MustClose()
 	page := stealth.MustPage(browser)
+	page.MustSetViewport(1920, 1080, 1, false)
+	page.MustSetUserAgent(&proto.NetworkSetUserAgentOverride{
+		UserAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+	})
 	page = page.MustNavigate(site)
 	fmt.Println("Начинаем работу...")
 	return browser, page
