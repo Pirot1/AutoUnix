e87d5d88cd6027a397c44bb44a5e9b546d3ae561
NikolaT <sitvaldyev.timur@gmail.com>
2026-03-23 00:12:00 +0500

    Add: Add .pdf reader

--- internal/parser/caption.go
+++ internal/parser/caption.go
@@ -2,11 +2,17 @@ package parser
 
 import (
 	"AutoUnix/internal/ai"
+	"bytes"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
+	"time"
+
+	"rsc.io/pdf"
 
 	"github.com/go-rod/rod"
 )
@@ -27,7 +33,75 @@ func Clean_subs(subtitle string) string {
 	}
 	return strings.Join(cleanLines, "\n")
 }
-
+func Make_conspect(lessonName string, fullText string) {
+	re := regexp.MustCompile(`[<>:"/\|?*]`)
+	name := re.ReplaceAllString(lessonName, "")
+	folderPath := filepath.Join("lessons", strings.TrimSpace(name))
+	err := os.MkdirAll(folderPath, 0755)
+	if err != nil {
+		fmt.Println("Ошибка создания папки:", err)
+		return
+	}
+	filePath := filepath.Join(folderPath, "lesson_summary.txt")
+	os.WriteFile(filePath, []byte(fullText), 0644)
+	// Ai-power caption
+	aiText := ai.Make_AI_conspect(fullText)
+	filePath = filepath.Join(folderPath, "lesson_AI_summary.txt")
+	err = os.WriteFile(filePath, []byte(Clean_subs(aiText)), 0644)
+	if err != nil {
+		fmt.Printf("Ошибка при сохранении субтитров: %v\n", err)
+	} else {
+		fmt.Printf("Файл сохранен: %s\n", filePath)
+	}
+}
+func DownloadAndProcessPDF(url string, lessonName string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Ошибка скачивания PDF:", err)
+		return
+	}
+	defer resp.Body.Close()
+	data, _ := io.ReadAll(resp.Body)
+	r, err := pdf.NewReader(bytes.NewReader(data), int64(len(data)))
+	if err != nil {
+		fmt.Println("Ошибка чтения PDF структуры:", err)
+		return
+	}
+	var fullText string
+	totalPage := r.NumPage()
+	for i := 1; i <= totalPage; i++ {
+		p := r.Page(i)
+		if p.V.IsNull() {
+			continue
+		}
+		content := p.Content()
+		for _, text := range content.Text {
+			fullText += text.S + " "
+		}
+		fullText += "\n\n"
+	}
+	Make_conspect(lessonName, fullText)
+}
+func Check_materials(page *rod.Page, lessonName string) {
+	exists, el, err := page.Has("track[kind='captions']")
+	if err != nil {
+		fmt.Printf("Ошибка при поиске: %v\n", err)
+		return
+	}
+	if !exists {
+		fmt.Println("Материалов на этом уроке нет.")
+		Check_materials(page, lessonName)
+		return
+	}
+	el.MustClick()
+	pdfEl, err := page.Timeout(2 * time.Second).Element("a[href$='.pdf']")
+	if err == nil {
+		pdfURL := pdfEl.MustAttribute("href")
+		fmt.Printf("Нашел PDF-конспект: %s\n", *pdfURL)
+		DownloadAndProcessPDF(*pdfURL, lessonName)
+		return
+	}
+}
 func Caption_recorder(page *rod.Page, lessonName string) {
 	exists, el, err := page.Has("track[kind='captions']")
 	if err != nil {
@@ -36,17 +110,9 @@ func Caption_recorder(page *rod.Page, lessonName string) {
 	}
 	if !exists {
 		fmt.Println("Субтитров на этом уроке нет.")
+		Check_materials(page, lessonName)
 		return
 	}
-	re := regexp.MustCompile(`[<>:"/\|?*]`)
-	name := re.ReplaceAllString(lessonName, "")
-	folderPath := filepath.Join("lessons", strings.TrimSpace(name))
-	err = os.MkdirAll(folderPath, 0755)
-	if err != nil {
-		fmt.Println("Ошибка создания папки:", err)
-		return
-	}
-	filePath := filepath.Join(folderPath, "lesson_summary.txt")
 	subtitleURL := el.MustAttribute("src")
 	if subtitleURL == nil || *subtitleURL == "" {
 		fmt.Println("У тега track нет ссылки src")
@@ -54,20 +120,5 @@ func Caption_recorder(page *rod.Page, lessonName string) {
 	}
 	fmt.Println("Начал запись субтитров...")
 	content := page.MustEval(`(url) => fetch(url).then(res => res.text())`, *subtitleURL).String()
-	err = os.WriteFile(filePath, []byte(Clean_subs(content)), 0644)
-	if err != nil {
-		fmt.Printf("Ошибка при сохранении субтитров: %v\n", err)
-	} else {
-		fmt.Printf("Файл сохранен: %s\n", filePath)
-	}
-
-	// Ai-power caption
-	aiText := ai.Make_AI_conspect(content)
-	filePath = filepath.Join(folderPath, "lesson_AI_summary.txt")
-	err = os.WriteFile(filePath, []byte(Clean_subs(aiText)), 0644)
-	if err != nil {
-		fmt.Printf("Ошибка при сохранении субтитров: %v\n", err)
-	} else {
-		fmt.Printf("Файл сохранен: %s\n", filePath)
-	}
+	Make_conspect(lessonName, content)
 }
