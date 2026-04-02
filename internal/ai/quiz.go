f80bff26f25d7a3625a7255fff501c771685dcda
Timur <122035339+Pirot1@users.noreply.github.com>
2026-04-02 11:23:58 +0500

    Fix: Rewrite main logic due to optimisation

--- internal/ai/quiz.go
+++ internal/ai/quiz.go
@@ -13,22 +13,27 @@ func SolvingQuiz(page *rod.Page) {
 	btn := page.MustActivate().MustElementX("//button[@class=\"text-white text-[15px] font-bold whitespace-nowrap\"]").MustWaitVisible()
 	btn.MustClick()
 	log.Println("Начинаю тест...")
+	var test []string
 	for i := 0; i < 5; i++ {
+		quest := fmt.Sprintf(`//div[@class="flex flex-row overflow-x-auto"]/div[%d]`, i+1)
+		page.MustElementX(quest).MustClick()
+		question := page.MustElementX("//span[@class=\"text-unix-text-black dark:text-[#AFB7CA] font-medium text-2xl\"]").MustText()
+		options := page.MustElementsX("//p[@class=\"ml-4\"]")
+		optionsList := ""
+		for i, opt := range options {
+			optionsList += fmt.Sprintf("%d. %s\n", i+1, opt.MustText())
+		}
+		test = append(test, fmt.Sprintf("Вопрос №%d: %s\nВарианты:\n%s\n", i+1, question, optionsList))
+	}
+	var answers []int
+	answers = AskGemini_Web(test[:]) //тут либо AskGemini_Web - без API или AskGemini - c API
+	for i := 0; i < 5; i++ {
+		answer := answers[i]
 		log.Printf("Вопрос №%d\n", i+1)
 		quest := fmt.Sprintf(`//div[@class="flex flex-row overflow-x-auto"]/div[%d]`, i+1)
 		page.MustElementX(quest).MustClick()
-		time.Sleep(500 * time.Millisecond)
-		question := page.MustElementX("//span[@class=\"text-unix-text-black dark:text-[#AFB7CA] font-medium text-2xl\"]").MustText()
-		options := page.MustElementsX("//p[@class=\"ml-4\"]")
-		var quiz []string
-		fmt.Printf("%s\n", question)
-		for _, el := range options {
-			txt := el.MustText()
-			fmt.Println(txt)
-			quiz = append(quiz, txt)
-		}
-		answer := AskGemini_Web(question, quiz[:]) //тут либо AskGemini_Web - без API или AskGemini - c API
-		log.Printf("ИИ выбрал: %d\n", answer)
+		time.Sleep(1 * time.Second)
+		log.Printf("ИИ выбрал: %d", answer)
 		if answer == 0 {
 			answer = rand.Intn(3) + 1
 			log.Printf("ИИ не смог выбрать ответ, поэтому ответ был выбран случайно: %d\n", answer)
@@ -36,7 +41,6 @@ func SolvingQuiz(page *rod.Page) {
 		ans := fmt.Sprintf(`//div[@class="flex flex-col mt-5"]/div[%d]`, answer)
 		page.MustElementX(ans).MustClick()
 		log.Println("Успешно ответил!")
-		time.Sleep(500 * time.Millisecond)
 	}
 	log.Println("Все вопросы пройдены. Завершаю тест...")
 	page.MustElementX(`//button[@class="flex items-center text-white text-[15px] font-bold whitespace-nowrap max-sm:whitespace-normal"]`).MustClick()
