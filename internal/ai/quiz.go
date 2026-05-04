9903ee8b6db11053e248be61a7ab8733b5fb7471
Timur <122035339+Pirot1@users.noreply.github.com>
2026-05-04 15:06:46 +0500

    Fix: Fix parser function

--- internal/ai/quiz.go
+++ internal/ai/quiz.go
@@ -12,9 +12,9 @@ import (
 func SolvingQuiz(page *rod.Page) {
 	btn := page.MustActivate().MustElementX("//button[@class=\"text-white text-[15px] font-bold whitespace-nowrap\"]").MustWaitVisible()
 	btn.MustClick()
-	log.Println("Starting up...")
+	log.Println("Starting test...")
+	var test []string
 	for i := 0; i < 5; i++ {
-		log.Printf("Question №%d\n", i+1)
 		quest := fmt.Sprintf(`//div[@class="flex flex-row overflow-x-auto"]/div[%d]`, i+1)
 		page.MustElementX(quest).MustClick()
 		question := page.MustElementX("//span[@class=\"text-unix-text-black dark:text-[#AFB7CA] font-medium text-2xl\"]").MustText()
@@ -23,20 +23,28 @@ func SolvingQuiz(page *rod.Page) {
 		for i, opt := range options {
 			optionsList += fmt.Sprintf("%d. %s\n", i+1, opt.MustText())
 		}
-		answer := AskGemini_Web(question, quiz[:]) //use API if you want
-		log.Printf("Ai chose: %d\n", answer)
+		test = append(test, fmt.Sprintf("Question №%d: %s\nVariants:\n%s\n", i+1, question, optionsList))
+	}
+	var answers []int
+	answers = AskGemini_Web(test[:]) //use API if you want
+	for i := 0; i < 5; i++ {
+		answer := answers[i]
+		log.Printf("Question №%d\n", i+1)
+		quest := fmt.Sprintf(`//div[@class="flex flex-row overflow-x-auto"]/div[%d]`, i+1)
+		page.MustElementX(quest).MustClick()
+		time.Sleep(1 * time.Second)
+		log.Printf("AI choose: %d", answer)
 		if answer == 0 {
 			answer = rand.Intn(3) + 1
-			log.Printf("AI couldn't find an answer, therefore the answer was chosen randomly: %d\n", answer)
+			log.Printf("AI couldn't generate an answer, therefore the answer was picked randomly: %d\n", answer)
 		}
 		ans := fmt.Sprintf(`//div[@class="flex flex-col mt-5"]/div[%d]`, answer)
 		page.MustElementX(ans).MustClick()
 		log.Println("Answer successfully!")
-		time.Sleep(500 * time.Millisecond)
 	}
-	log.Println("All question are done. Finishing up...")
+	log.Println("All question was done. Finishing the test up...")
 	page.MustElementX(`//button[@class="flex items-center text-white text-[15px] font-bold whitespace-nowrap max-sm:whitespace-normal"]`).MustClick()
 	page.MustElementX(`//div[@class="flex-1 py-3 mr-1 border border-[#5F6B88] text-[#5F6B88] rounded-lg cursor-pointer text-[15px] font-bold text-center"]`).MustClick()
 	grade := page.MustElementX(`//div[@class="flex items-center max-sm:justify-center"]/p/span[1]`).MustText()
-	fmt.Printf("Your grade: %s\n", grade) //!!!!
+	fmt.Printf("Your grade: %s\n", grade)
 }
